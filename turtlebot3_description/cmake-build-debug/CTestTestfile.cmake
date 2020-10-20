3144b064d0a7fc92c0b358fbabd79cfbbcbc27d4
Heimv <victor.heim@epitech.eu>
2020-10-20 15:43:24 +0200

    removing turtlebot_description repo to avoid duplicate repo while importing

--- turtlebot3_description/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/heimv/CLionProjects/ROB1---Rosort/src/turtlebot3_description
-# Build directory: /home/heimv/CLionProjects/ROB1---Rosort/src/turtlebot3_description/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
